dee28e08607c3920a6ae4ca236faf4947b37bb44
allansham <allan.sham@trapezegroup.com>
2020-11-17 15:51:23 +0000

    Remove persist state for now

--- src/main.ts
+++ src/main.ts
@@ -6,17 +6,17 @@ import { environment } from './environments/environment';
 import { persistState } from '@datorama/akita';
 import { debounceTime } from 'rxjs/operators';
 
-const storage = persistState({
-  include: ['routed-tab'],
-  preStorageUpdateOperator: () => debounceTime(2000), // update the localstorage 2 seconds after a change have been made, to reduce writes to localstorage
-});
+// const storage = persistState({
+//   include: ['routed-tab'],
+//   preStorageUpdateOperator: () => debounceTime(2000), // update the localstorage 2 seconds after a change have been made, to reduce writes to localstorage
+// });
 
-const providers = [{ provide: 'persistStorage', useValue: storage }];
+// const providers = [{ provide: 'persistStorage', useValue: storage }];
 
 if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic(providers)
+platformBrowserDynamic()
   .bootstrapModule(AppModule)
   .catch((err) => console.error(err));
